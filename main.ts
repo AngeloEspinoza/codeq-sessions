f3a4f4d355a2c06dee9c1a2cca4f4e4b9ed8fee9
Angelo Espinoza <angelodavid98@hotmail.com>
2024-09-05 13:31:19 -0600

    Minor changes

--- main.ts
+++ main.ts
@@ -2,6 +2,5 @@
 //  Autor: Angelo Espinoza
 //  Fecha de creación: 5/9/2024
 //  Descripción: Este programa enciende un LED del microcontrolador MICRO:BIT
-//  Primera manera
 //  Enciende un led en la posición (x, y)
 led.plot(0, 0)
