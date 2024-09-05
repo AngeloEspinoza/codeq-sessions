ba1e708260b3ebf3bd5748735108afc50f696345
Angelo Espinoza <angelodavid98@hotmail.com>
2024-09-05 13:30:54 -0600

    Refactor blinking LED

--- main.ts
+++ main.ts
@@ -1,1 +1,7 @@
-
+//  Encender un led
+//  Autor: Angelo Espinoza
+//  Fecha de creación: 5/9/2024
+//  Descripción: Este programa enciende un LED del microcontrolador MICRO:BIT
+//  Primera manera
+//  Enciende un led en la posición (x, y)
+led.plot(0, 0)
