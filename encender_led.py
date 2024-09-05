6d4d0e0d3465cb4c29d9c095fd6d54cc7f3604cc
Angelo Espinoza <angelodavid98@hotmail.com>
2024-09-05 13:05:51 -0600

    Add blinking LED script

---
+++ encender_led.py
@@ -0,0 +1,14 @@
+# Encender un led
+
+# Autor: Angelo Espinoza
+# Fecha de creación: 5/9/2024
+# Descripción: Este programa enciende un LED del microcontrolador MICRO:BIT
+
+# Enciende uno o más LEDs dependiendo si es punto (.) o gato (#)
+basic.show_leds("""
+. . . . .
+. . . . .
+. . # . .
+. . . . .
+. . . . .
+""")
--- encender_led.ts
+++ encender_led.ts
@@ -1,1 +1,14 @@
-
+namespace encender_led {
+    //  Encender un led
+    //  Autor: Angelo Espinoza
+    //  Fecha de creación: 5/9/2024
+    //  Descripción: Este programa enciende un LED del microcontrolador MICRO:BIT
+    //  Enciende uno o más LEDs dependiendo si es punto (.) o gato (#)
+    basic.showLeds(`
+. . . . .
+. . . . .
+. . # . .
+. . . . .
+. . . . .
+`)
+}
